7f375d311f37e2b7f0f96e57de59fe3e86a0cc94
Shaja Nasir <Shajanasir@gmail.com>
2023-07-12 17:39:35 +0530

    Create arrow func comp with css files

---
+++ src/components/Pages/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./LoginPage.module.css";
+
+const LoginPage = (props) => {
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
---
+++ src/components/Pages/SignupPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./SignupPage.module.css";
+const SignupPage = () => {
+  return <div>SignupPage</div>;
+};
+
+export default SignupPage;
